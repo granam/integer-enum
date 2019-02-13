b62eb6c1b147df7a8382238945747ec923dd4582
Jaroslav Týc <mail@jaroslavtyc.com>
2019-02-13 09:52:16 +0100

    Enumeration with integers only

---
+++ Granam/IntegerEnum/Exceptions/UnexpectedValueToConvert.php
@@ -0,0 +1,9 @@
+<?php
+namespace Granam\IntegerEnum\Exceptions;
+
+use Granam\ScalarEnum\Exceptions\UnexpectedValueToEnum;
+
+class UnexpectedValueToConvert extends UnexpectedValueToEnum implements Logic
+{
+
+}
